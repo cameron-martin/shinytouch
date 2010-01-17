3f011d7dc98e63fdef32a1c37e026b68c4f2ec16
Cameron <ddlshack@gmail.com>
2010-01-17 02:58:29 +0000

    Ignored config_warp.py

--- config_warp.py
+++ config_warp.py
@@ -1,3 +1,3 @@
 # Points to warp to
 # top left, top right, bottom right, bottom left
-warp_points = [[393, 122], [521, 47], [536, 400], [400, 353]]
+warp_points = [[75, 116], [551, 138], [504, 462], [166, 376]]
