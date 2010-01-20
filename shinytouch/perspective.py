e8c1a46e3b8442ef7a4d20057ecddc84c9919fc1
ddlshack <ddlshack@gmail.com>
2010-01-20 04:45:28 -0700

    `Made Perspective Modular

--- shinytouch/perspective.py
+++ shinytouch/perspective.py
@@ -1,32 +1,33 @@
-class Perspective:
     # Set up the perspective matrix
-    def __init__(self):
-        self.update_matrix()
+    import config
+    from opencv.cv import *
+    mmat = False
+    self.update_matrix()
 
-    def warp(self, frame):
-        self.frame2 = cvCreateImage(cvSize(width, height), IPL_DEPTH_8U, 3)
-        cvWarpPerspective(frame, self.frame2, self.mmat)
-        return self.frame2
+    def warp(frame):
+        global mmat
+        frame2 = cvCreateImage(cvGetSize(frame), frame.depth, frame.nChannels)
+        cvWarpPerspective(frame, frame2, mmat)
+        return frame2
 
-    def update_matrix(self):
-        global height, width, warp_points
-
-        # Create Warp Map
-        self.mmat = cvCreateMat(3,3,CV_32FC1)
-
+    def update_matrix():
+        global mmat
+        
+        mmat = cvCreateMat(3,3,CV_32FC1)
+        
         # Set up source points
-        self.storageSrc = cvCreateMemStorage(0)
-        self.src = cvCreateSeq(CV_SEQ_ELTYPE_POINT, sizeof_CvContour, sizeof_CvPoint, self.storageSrc)
-        self.src = CvSeq_CvPoint2D32f_cast(self.src)
-        self.srcp = [ (warp_points[0][0], warp_points[0][1]), (warp_points[3][0], warp_points[3][1]), (warp_points[1][0], warp_points[1][1]), (warp_points[2][0], warp_points[2][1]) ]
-        [cvSeqPush (self.src, cvPoint2D32f(*p)) for p in self.srcp]
+        storageSrc = cvCreateMemStorage(0)
+        src = cvCreateSeq(CV_SEQ_ELTYPE_POINT, sizeof_CvContour, sizeof_CvPoint, storageSrc)
+        src = CvSeq_CvPoint2D32f_cast(src)
+        srcp = [ (warp_points[0][0], warp_points[0][1]), (warp_points[3][0], warp_points[3][1]), (warp_points[1][0], warp_points[1][1]), (warp_points[2][0], warp_points[2][1]) ]
+        [cvSeqPush (src, cvPoint2D32f(*p)) for p in srcp]
 
         # Now setup DESTINATION points (into dst)
-        self.storageDst = cvCreateMemStorage(0)
-        self.dst = cvCreateSeq(CV_SEQ_ELTYPE_POINT, sizeof_CvContour, sizeof_CvPoint, self.storageDst)
-        self.dst = CvSeq_CvPoint2D32f_cast(self.dst)
-        self.dstp = [ (0, 0), (0, height), (width, 0), (width, height) ]
-        [cvSeqPush(self.dst, cvPoint2D32f(*p)) for p in self.dstp]
+        storageDst = cvCreateMemStorage(0)
+        dst = cvCreateSeq(CV_SEQ_ELTYPE_POINT, sizeof_CvContour, sizeof_CvPoint, storageDst)
+        dst = CvSeq_CvPoint2D32f_cast(self.dst)
+        dstp = [ (0, 0), (0, config.height), (config.width, 0), (config.width, config.height) ]
+        [cvSeqPush(dst, cvPoint2D32f(*p)) for p in dstp]
 
         # Calculate transform MATRIX
-        cvGetPerspectiveTransform(self.src[0], self.dst[0], self.mmat)
+        cvGetPerspectiveTransform(src[0], dst[0], mmat)
