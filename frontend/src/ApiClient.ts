3a6a7f6da0e22535edbc73c58010662f09556c43
Cameron Martin <cameronmartin123@gmail.com>
2019-04-14 01:33:34 +0100

    Changed api host when deploying

--- frontend/src/ApiClient.ts
+++ frontend/src/ApiClient.ts
@@ -1,9 +1,11 @@
+const API_HOST = process.env.API_HOST || 'http://localhost:5678';
+
 export default class ApiClient {
   /**
    * @param image An image encoded using JPEG
    */
   async addExample(image: Blob): Promise<void> {
-    const response = await fetch("http://localhost:5678/add-example", {
+    const response = await fetch(`${API_HOST}/add-example`, {
       method: "POST",
       body: image,
       headers: {
